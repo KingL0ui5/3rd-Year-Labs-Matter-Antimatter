668c897326fec8383267e027224ed77e457c2f14
Tomàs Aspa Mayor <tomas.aspa05@gmail.com>
2026-01-09 16:31:05 +0000

    Matched dimuon peaks to J/psi states.

---
+++ correlations.py
@@ -0,0 +1,10 @@
+# Looking at how two features correlate with each other
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import histograms
+
+def plot_correlations(data, feature_x, feature_y):
+    data[feature_x]
---
+++ model_ml.py
@@ -0,0 +1,13 @@
+## Apply the ML methods to the split datasets.
+
+import xgboost
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import roc_curve
+
+import histograms
+
+# Take a look at the datasets and see how they separate
+sig, bkg = histograms.data()
+
+
