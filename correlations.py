9b4dc9801384856fb8b05775a02b162833eabc81
Louis Liu <liulouis1@gmail.com>
2026-01-13 11:24:27 +0000

    correlation done

--- correlations.py
+++ correlations.py
@@ -5,30 +5,44 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+
 def plot_correlation_matrix(data):
     correlation_matrix = data.corr()
     plt.figure(figsize=(10, 8))
-    sns.heatmap(correlation_matrix, fmt=".2f", cmap='coolwarm', square=True, xticklabels=False, yticklabels=False)
+    sns.heatmap(correlation_matrix, fmt=".2f", cmap='coolwarm',
+                square=True, xticklabels=False, yticklabels=False)
     plt.title('Feature Correlation Heatmap')
-    plt.savefig('figs/correlation_matrix.png')
+    # plt.savefig('figs/correlation_matrix.png')
     plt.show()
 
 # plot the correlation matrix for B invariant mass
+
+
 def plot_Binv_correlation(data):
     correlation_matrix = data.corrwith(data['B invariant mass']).to_frame()
     plt.figure(figsize=(10, 8))
-    sns.heatmap(correlation_matrix, fmt=".2f", cmap='coolwarm', square=True, xticklabels=False, yticklabels=False)
+    sns.heatmap(correlation_matrix, fmt=".2f", cmap='coolwarm',
+                square=True, xticklabels=False, yticklabels=False)
     plt.title('B invariant mass correlations')
-    plt.savefig('figs/B_inv_correlation_matrix.png')
+    # plt.savefig('figs/B_inv_correlation_matrix.png')
     plt.show()
+    return correlation_matrix.sort_values(by=0, ascending=False)
 
-# drop all columns that correlate highly with target: "B invariant mass"
-def drop_correlated(target, data, threshold=0.9):
+
+#  drop all columns that correlate highly with target: "B invariant mass"
+
+
+def drop_correlated(target, data, threshold=0.5):
     correlations = data.corr().abs()[target]
-    to_drop = correlations[(correlations > threshold) & (correlations.index != target)].index
+    to_drop = correlations[((correlations > threshold) &
+                           (correlations.index != target)) | correlations.isna()].index
     print(f"Dropping {len(to_drop)} columns: {list(to_drop)}")
     return data.drop(columns=to_drop)
 
+
 if __name__ == "__main__":
     data = pd.read_pickle('data/dataset_2011.pkl')
-    drop_correlated('B invariant mass', data)
+    correlations = plot_Binv_correlation(data)
+    data_cleaned = drop_correlated('B invariant mass', data, threshold=0.5)
+
+    print(correlations.to_string(), file=open("output.txt", "w"))
--- model_ml.py
+++ model_ml.py
@@ -1,11 +1,99 @@
-# Apply the ML methods to the split datasets.
+"""
+Using XGBoost to classify signal and background events.
+Louis Liu 12/01
+"""
 
+from matplotlib import pyplot as plt
+import pandas
 import xgboost
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import roc_curve
+import raw_data
+from correlations import drop_correlated
 
-import histograms
+data_raw = raw_data.data()
+sig, bkg = drop_correlated('B invariant mass', data_raw, threshold=0.7)
 
-# Take a look at the datasets and see how they separate
-sig, bkg = histograms.data()
+
+plt.hist(sig, bins=100, label='Signal')
+plt.hist(bkg, bins=100, label='Background')
+plt.xlabel(r'var1')
+plt.ylabel(r'Candidates$')
+plt.legend()
+plt.show()
+
+nSignalTotal = sig.count()
+nBackgroundTotal = bkg.count()
+nSignalSelected = sig[sig <= 0].count()
+nBackgroundSelected = bkg[bkg <= 0].count()
+
+print('Signal Efficiency     = {}%'.format(100*nSignalSelected/nSignalTotal))
+print('Background Efficiency = {}%'.format(
+    100*nBackgroundSelected/nBackgroundTotal))
+
+plt.hist(sig, bins=100, label='Signal')
+plt.hist(bkg, bins=100, label='Background')
+plt.xlabel(r'var2')
+plt.ylabel(r'Candidates$')
+plt.legend()
+plt.show()
+plt.hist(sig, bins=100, label='Signal')
+plt.hist(bkg, bins=100, label='Background')
+plt.xlabel(r'var3')
+plt.ylabel(r'Candidates$')
+plt.legend()
+plt.show()
+
+
+plt.scatter(sig, sig, label='Signal')
+plt.scatter(bkg, bkg, label='Background')
+plt.xlabel(r'var2')
+plt.ylabel(r'var3')
+plt.legend()
+plt.show()
+
+model = xgboost.XGBClassifier(eval_metric='auc', early_stopping_rounds=50,
+                              n_estimators=1000, learning_rate=0.3, max_depth=6)
+
+
+# %% Train Model
+nSignalTrain = int(0.9*nSignalTotal)
+nSignalTest = nSignalTotal - nSignalTrain
+nBackgroundTrain = int(0.9*nBackgroundTotal)
+nBackgroundTest = nBackgroundTotal - nBackgroundTrain
+
+# x are the features and y are the classes
+x_train = pandas.concat(
+    [sig[:nSignalTrain], bkg[:nBackgroundTrain]], axis=0, ignore_index=True)
+y_train = [1]*nSignalTrain + [0]*nBackgroundTrain
+
+x_test = pandas.concat(
+    [sig[nSignalTrain:], bkg[nBackgroundTrain:]], axis=0, ignore_index=True)
+y_test = [1]*nSignalTest + [0]*nBackgroundTest
+
+model.fit(x_train, y_train, eval_set=[(x_test, y_test)])
+
+# %% Evaluate Model
+prediction = model.predict_proba(x_test)
+
+plt.hist([p[1] for p, cls in zip(prediction, y_test)
+         if cls == 1], bins=100, label='Signal')
+plt.hist([p[1] for p, cls in zip(prediction, y_test)
+         if cls == 0], bins=100, label='Background')
+plt.xlabel(r'Signal Probability')
+plt.ylabel(r'Candidates$')
+plt.legend()
+plt.show()
+
+# %% ROC Curve
+background_accepted, signal_accepted, probabilities_tested = roc_curve(
+    y_test, prediction[:, 1])
+plt.plot(background_accepted, signal_accepted, label='ROC curve')
+plt.plot([0, 1], [0, 1], color='red', linestyle='--', label='Random')
+plt.xlabel(r'Background Contamination')
+plt.ylabel(r'Signal Efficiency')
+plt.legend()
+plt.show()
+
+a = xgboost.plot_importance(model)
