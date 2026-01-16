62e4507ac0ba297935a4fcc642c9bcac3fe365da
Louis Liu <liulouis1@gmail.com>
2026-01-16 10:50:24 +0000

    fixes

--- filtered_data.py
+++ filtered_data.py
@@ -15,25 +15,26 @@ sns.set_palette("colorblind")
 
 
 with open('datasets/dataset_2011.pkl', 'rb') as infile:
-    __data_2011 = pickle.load(infile)
+    _data_2011 = pickle.load(infile)
 
-__data_2011.insert(0, 'index', range(len(__data_2011)))
+_data_2011.insert(0, 'index', range(len(_data_2011)))
 
 
 def raw_data():
     """
     Return a copy of the full 2011 dataset.
     """
-    return __data_2011.copy()
+    return _data_2011.copy()
 
 
 def samesign():
-    data = __data_2011['Same-sign muon assumed particle type']
+    data = _data_2011['Same-sign muon assumed particle type']
     hist = plt.hist(data, bins=150)
     plt.xlabel(r'Same-sign muon invariant mass / MeV/$c^2$')
     plt.ylabel(r'Candidates / (23 MeV/$c^2)$')
     plt.show()
     return data
+
 
 class seperate:
     def __init__(self, k: int = None, plot: bool = False):
@@ -50,7 +51,7 @@ class seperate:
         # dataset = drop_correlated('B invariant mass', __data_2011, threshold=0.5)
         self.__datasets = []
 
-        dataset = __data_2011.copy()
+        dataset = _data_2011.copy()
 
         signal = dataset[dataset['dimuon-system invariant mass'].between(3070, 3200) |
                          dataset['dimuon-system invariant mass'].between(3600, 3750)]
@@ -96,9 +97,19 @@ class seperate:
         """
         signal = self.__signal_parts
         background = self.__background_parts
+
         if drop_cols:
-            signal = signal.drop(columns=drop_cols)
-            background = background.drop(columns=drop_cols)
+            if isinstance(signal, list):
+                signal = [df.drop(columns=drop_cols, errors='ignore')
+                          for df in signal]
+                background = [df.drop(columns=drop_cols, errors='ignore')
+                              for df in background]
+
+            else:
+                signal = signal.drop(columns=drop_cols, errors='ignore')
+                background = background.drop(
+                    columns=drop_cols, errors='ignore')
+
         return signal, background
 
     def dataset_k(self, k):
@@ -110,27 +121,27 @@ class seperate:
 
 # %% Initial B invariant mass filtering
 def __task1():
-    hist = plt.hist(__data_2011['B invariant mass'], bins=100)
+    hist = plt.hist(_data_2011['B invariant mass'], bins=100)
     peaks = find_peaks(hist[0], height=1e5)[0]
-    plt.vlines(__data_2011['B invariant mass'][peaks], 0, max(hist[0]),
+    plt.vlines(_data_2011['B invariant mass'][peaks], 0, max(hist[0]),
                colors='r', linestyles='dashed', label='peaks')
     plt.xlabel(r'B candidate mass / MeV/$c^2$')
     plt.ylabel(r'Candidates / (23 MeV/$c^2)$')
     plt.show()
 
     print(
-        f"B invariant mass peaks: {__data_2011['B invariant mass'][peaks].values}")
+        f"B invariant mass peaks: {_data_2011['B invariant mass'][peaks].values}")
 
     #  remove charm anticharm meson J/psi (dominanat interaction)
-    plt.hist(__data_2011[abs(__data_2011['dimuon-system invariant mass'] -
-                             3097) > 100]['B invariant mass'], bins=100)
+    plt.hist(_data_2011[abs(_data_2011['dimuon-system invariant mass'] -
+                            3097) > 100]['B invariant mass'], bins=100)
     plt.xlabel(r'B candidate mass / MeV/$c^2$')
     plt.ylabel(r'Candidates / (23 MeV/$c^2)$')
     plt.show()
 
     #  remove charm anticharm meson J/psi and psi(2S) (other dominanat interaction)
-    plt.hist(__data_2011[(abs(__data_2011['dimuon-system invariant mass'] - 3097) > 100) &
-                         (abs(__data_2011['dimuon-system invariant mass'] - 3686) > 100)]['B invariant mass'], bins=100)
+    plt.hist(_data_2011[(abs(_data_2011['dimuon-system invariant mass'] - 3097) > 100) &
+                        (abs(_data_2011['dimuon-system invariant mass'] - 3686) > 100)]['B invariant mass'], bins=100)
     plt.xlabel(r'B candidate mass / MeV/$c^2$')
     plt.ylabel(r'Candidates / (23 MeV/$c^2)$')
     plt.show()
@@ -139,11 +150,11 @@ def __task1():
 
 
 def __task2():
-    hist = plt.hist(__data_2011['dimuon-system invariant mass'], bins=100)
+    hist = plt.hist(_data_2011['dimuon-system invariant mass'], bins=100)
 
     peaks = find_peaks(hist[0], height=1e3, distance=1, prominence=50)[0]
 
-    plt.vlines(__data_2011['dimuon-system invariant mass'][peaks], 0, max(hist[0]),
+    plt.vlines(_data_2011['dimuon-system invariant mass'][peaks], 0, max(hist[0]),
                colors='r', linestyles='dashed', label='peaks')
 
     plt.xlabel(r'Dimuon invariant mass / MeV/$c^2$')
@@ -151,4 +162,4 @@ def __task2():
     plt.show()
 
     print(
-        f"Invariant mass peaks: {__data_2011['dimuon-system invariant mass'][peaks].values}")
+        f"Invariant mass peaks: {_data_2011['dimuon-system invariant mass'][peaks].values}")
